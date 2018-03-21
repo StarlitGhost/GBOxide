804f27992fa70756cc0fea7285200ae10a654c68
StarlitGhost <36834781+StarlitGhost@users.noreply.github.com>
2018-03-21 12:10:21 +0000

    Add set/get for interrupt flag and enable

--- src/gameboy/interrupt.rs
+++ src/gameboy/interrupt.rs
@@ -18,6 +18,21 @@ impl InterruptHandler {
             enable: 0x00,
         }
     }
+
+    pub fn get_flag(&self) -> u8 {
+        self.flag
+    }
+    pub fn set_flag(&mut self, value: u8) {
+        self.flag = value;
+    }
+
+    pub fn get_enable(&self) -> u8 {
+        self.enable
+    }
+    pub fn set_enable(&mut self, value: u8) {
+        self.enable = value;
+    }
+
     pub fn set_interrupt(&mut self, interrupt: Interrupt) {
         self.flag |= interrupt as u8;
     }
